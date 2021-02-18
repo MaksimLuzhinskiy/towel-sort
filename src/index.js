5cd603636ddccd7a62d748ffe3876804845938de
MaksimLuzhinskiy <maks.luzhinskiy00@gmail.com>
2021-02-18 20:51:54 +0300

    fetch: add func

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    
+     let arr = new Array();
+      if(!Array.isArray(matrix)){ return arr}
+      matrix.map((x,i)=>{
+          if((i+1) %2 == 0 ){arr=arr.concat(x.reverse())}else{arr=arr.concat(x);}
+      });
+      return arr;
 }
